f934c5a6af42076e33985060d12fccbfc672cee0
Yazan Diranieh <yazanmd@yahoo.com>
2021-05-20 01:13:46 +0100

    Multiple servers with a waitgroup to synchronize goroutines.

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
 /* To build and run
-	go build
-	go run .
-	open a browser on: http://127.0.0.1:9000
- */
+go build
+go run .
+open a browser on: http://127.0.0.1:9000
+*/
 func main() {
-	// Register a few routes
+	// Create a wait group (count down) to wait for two http servers to terminate
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(2)
+
+	// Register a few routes (shared by both server, for now)
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		// Get the response header
 		header := writer.Header()
@@ -30,16 +36,34 @@ func main() {
 		fmt.Fprint(writer, `{"status":"OK"}`)
 	})
 	http.HandleFunc("/help", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer,"This is the help page. Time now: %s", time.Now().Format(time.RFC822))
+		fmt.Fprintf(writer, "This is the help page. Time now: %s", time.Now().Format(time.RFC822))
 	})
 
 	// Handler to server a file system directory
 	var fileSystem = http.Dir("C:\\Projects_Go\\SimpleHttpServer")
-	var fileServer = http.FileServer(fileSystem);
-	http.Handle("/files/", http.StripPrefix( "/files", fileServer ))		// requires a trailing /
+	var fileServer = http.FileServer(fileSystem)
+	http.Handle("/files/", http.StripPrefix("/files", fileServer)) // requires a trailing /
 
+	// Start one server
+	go func() {
+		// Listen in on port 9000 on any address
+		err := http.ListenAndServe(":9000", nil)
+		if err != nil {
+			log.Printf("Server at port 9000 failed: %s\n", err.Error())
+		}
+		waitGroup.Done()
+	}()
 
-	// Listen in on port 9000 on any address and use our simple handler
-	// which implements the Handler interface
-	http.ListenAndServe(":9000", nil)
+	// Start another server
+	go func() {
+		// Listen in on port 9000 on any address
+		err := http.ListenAndServe(":9001", nil)
+		if err != nil {
+			log.Printf("Server at port 9001 failed: %s\n", err.Error())
+		}
+		waitGroup.Done()
+	}()
+
+	// Wait for both servers to terminate
+	waitGroup.Wait()
 }
